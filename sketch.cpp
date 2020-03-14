ecd3d17d976c1fd6a4661c67dd3485f388ba9f99
Rupinder Ghotra <rghotra@blackberry.com>
2020-03-14 17:17:25 -0400

    Restructuring of project
    Change-Id: I60bb5bd29b473e4000f071b9b7eddd0f7a19ecea

--- sketch.cpp
+++ sketch.cpp
@@ -1,20 +1,49 @@
-#include <string>
+#include <cstring>
 #include <iostream>
+#include <list>
+#include <unordered_map>
+#include <string>
+/* Details of data structures */
+struct Book {
+  std::list<Level*> levels;
+};
+
+struct Level {
+  double price;
+  int num_nodes;
+  std::list<LevelNode*> nodes;
+};
+
+struct LevelNode {
+  int id;
+  int size;
+  Level* level;
+};
 
 class OrderBook {
 private:
-  int orderId;
-  bool side;
-  double price;
-  int size;
+  Book ask_book;
+  Book bid_book;
+  // Hash table to get LevelNode* from id
+  std::unordered_map<int, std::list<LevelNode*>::iterator> order_map;
 public:
-  void add_order(int id, bool side, double price, int size);
+  void add_order(int id, bool side, double price, int size) {
+
+  }
   void cancel_order(int id);
 };
 
 class MessageParser {
+//ideal way to write
 public:
-  void parse(const string &input);
+  void parse(std::string  &input) {
+    //sohisticated validation need to be done here
+    //1. Check if you have 5 required parameters for add order and three paramters for cancel order
+    //2. Check types of each parameter
+    //3. Populate order book's buy and sell struct
+    //4. Run OrderMatching algorithm to see if there is a trade match
+
+  }
 };
 
 class ExecutionListener {
@@ -28,38 +57,8 @@ public:
 void main() {
   MessageParser parser;
   OrderBook book;
-
   std::string input;
   while(std::getline(std::cin, input)) {
     parser.parse(input);
   }
-};
-
-/* Details of data structures */
-
-struct Book;
-
-class OrderBook {
-private:
-  Book ask_book;
-  Book bid_book;
-
-  // Hash table to get LevelNode* from id
-  std::unordered_map<int, std::list<LevelNode*>::iterator> order_map;
-};
-
-struct LevelNode {
-  int id;
-  int size;
-  Level* level;
-};
-
-struct Level {
-  double price;
-  int num_nodes;
-  std::list<LevelNode*> nodes;
-};
-
-struct Book {
-  std::list<Level*> levels;
-};
+}
