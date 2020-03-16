4bc7c20a510330d4a7f1f35944cd6dd77f4a4860
Rupinder Ghotra <rghotra@blackberry.com>
2020-03-16 10:53:38 -0400

    Added iterators
    Change-Id: I2e16bd7bded060e33dfba6c1e43aa144f86034fd

--- OrderBook.h
+++ OrderBook.h
@@ -27,12 +27,19 @@ private:
 
     // Hash table to get LevelNode* from order id
     std::unordered_map<int, std::list<LevelNode*>::iterator> order_map;
+    void _updateAskBook() {
+
+    }
+
+    void _updateBidBook() {
+
+    }
     public:
         OrderBook() {
         }
         ~OrderBook() {};
-        //createBookNode->createLevelNodeList->createLevelNode
-        Level* createBookNode(int id, double price, int size) {
+        //createOrderNode->createLevelNodeList->createLevelNode
+        Level* createOrderNode(int id, double price, int size) {
             //when it will create level it will also create LevelNode for the entry itself.
             Level *levelPtr = new Level;
             levelPtr->price = price;
@@ -40,17 +47,22 @@ private:
             levelPtr->num_nodes = levelPtr->nodes.size();
             return levelPtr;
         }
-        //insertBookNode->createBookNode->createLevelNodeList->createLevelNode
-        void insertBookNode(int id, bool side, double price, int size) {
-            Level* level = createBookNode(id, price, size);
+        //insertBookNode->createOrderNode->createLevelNodeList->createLevelNode
+        void insertOrderNode(int id, bool side, double price, int size) {
+            Level* level = createOrderNode(id, price, size);
             if(side) {
                 ask_book.levels.push_back(level);
-                //add new order id into unordered map
-                
+                std::list<LevelNode*>::iterator iter = level->nodes.begin();
+                //order_map(id, iter);
             } else {
                 //bid_book insert
                 bid_book.levels.push_back(level);
             }
+        }
+
+        void updateOrderBook() {
+            _updateAskBook();
+            _updateBidBook();
         }
 
         //createLevelNodeList->createLevelNode
@@ -87,19 +99,40 @@ private:
         //iterate order book
         void iterateOrderBook(OrderBook &book) {
             if(book.ask_book.levels.size() != 0) {
+                std::cout << "Current SALE Ledger." << std::endl;
                 std::list<Level*>::iterator iter = book.ask_book.levels.begin();
+                while(iter != book.ask_book.levels.end()) {
+                    std::cout << "Level:: " << (*iter)->price << "\tNumber of Resting Orders:: " 
+                    << (*iter)->num_nodes << std::endl;
+                    iterateLevelNodeList((*iter)->nodes);
+                    std::cout << "==>==>==>==>==>" << std::endl;
+                    ++iter;
+                }
             }
             if(book.bid_book.levels.size() != 0) {
                 //iterate over bid_book
+                std::cout << "Current BUY Ledger." << std::endl;
                 std::list<Level*>::iterator iter = book.bid_book.levels.begin();
+                while(iter != book.bid_book.levels.end()) {
+                    std::cout << "Level:: " << (*iter)->price << "\tNumber of Resting Orders:: " 
+                    << (*iter)->num_nodes << std::endl;
+                    iterateLevelNodeList((*iter)->nodes);
+                    std::cout << "<==<==<==<==<==" << std::endl;
+                    ++iter;
+                }
             }
         }
 
         //iterate LevelNode
-        void iterateLevelNode(std::list<LevelNode*> levelNodeList) {
+        void iterateLevelNodeList(std::list<LevelNode*> levelNodeList) {
             if(levelNodeList.size()) {
                 //iterate over levelNodeList
                 std::list<LevelNode*>::iterator iter = levelNodeList.begin();
+                while(iter != levelNodeList.end()) {
+                    std::cout << "Order ID:: " << (*iter)->id << "\tSize:: " << (*iter)->size 
+                    << "\tParent Level:: " << (*iter)->level->price << std::endl;
+                    iter++;
+                }
             }
         }
 };
